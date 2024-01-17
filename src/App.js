d2fb7942a465510f8f483164625db7218628fae3
Elvina <rammnuschka@gmail.com>
2024-01-17 23:52:18 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Cart from "./Components/Cart/Cart";
-import Dish from './Components/DishesComponents/Dish';
 import Dishes from "./Components/DishesComponents/Dishes";
 import AllCategories from "./Components/Filter/AllCategories";
 
